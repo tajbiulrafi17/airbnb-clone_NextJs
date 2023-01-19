c37af641b2000dd05ebd9ac2e43135370fe0f7c9
Tajbiul Hassan Rafi <tajbiul.rafi@northsouth.edu>
2023-01-20 05:37:23 +0600

    Card and Footer

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
+import SmallCard from "@/components/SmallCrad";
+import MediumCard from "@/components/MediumCard";
+import LargeCard from "@/components/LargeCard";
+import Footer from "@/components/Footer";
 
-export default function Home() {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -11,9 +15,70 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* <h1>Lets build airbnb</h1> */}
+
       <Header />
       <Banner />
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        {/* Small Card */}
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Exprole Nearby</h2>
+          {/* Pull data from server - API endpoints  */}
+          <div
+            className="grid grid-cols-1 sm:grid-cols-2
+           lg:grid-cols-3 xl:grid-cols-4"
+          >
+            {exploreData?.map((item) => (
+              <SmallCard
+                key={item.img}
+                img={item.img}
+                distance={item.distance}
+                location={item.location}
+              />
+            ))}
+          </div>
+        </section>
+
+        {/* MediumCard */}
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+
+          <div
+            className="flex space-x-3 overflow-scroll
+          scrollbar-hide p-3 ml-3"
+          >
+            {cardsData?.map((item) => (
+              <MediumCard key={item.img} img={item.img} title={item.title} />
+            ))}
+          </div>
+        </section>
+
+        {/* Large Card */}
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists created by Airbnb."
+          buttonText="Get Inspired"
+        />
+      </main>
+
+      <Footer />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G").then(
+    (res) => res.json()
+  );
+
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
 }
